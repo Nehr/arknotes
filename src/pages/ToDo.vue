93fcd9b4b6870173e9eb594796fcf8dbc7cf6075
Thom <thom@websquid.se>
2021-08-05 14:43:24 +0200

    Updated to do-list

--- src/pages/ToDo.vue
+++ src/pages/ToDo.vue
@@ -5,10 +5,12 @@
       <p>
         Don't forget...<br>
         <ul>
-          <li>Edit button to show hidden notes</li>
-          <li>Sort items by name/note</li>
+          <li class="done">Edit button to show hidden notes</li>
+          <li class="done">Sort items by name/note</li>
+          <li class="done">Sort items from both long and lat, whichever is closer</li>
           <li>Enter utgångspunkt för zeroVal</li>
-          <li>Add map (canvas) with lon/lat dots for each unfinished note</li>
+          <li>Add map (canvas?) with lon/lat dots for each unfinished note</li>
+          <li>Hide sections when all items are hidden</li>
         </ul>
       </p>
     </div>
@@ -22,3 +24,13 @@ export default defineComponent({
   name: 'ToDo'
 })
 </script>
+
+<style lang="scss" scoped>
+  ul {
+    li {
+      &.done {
+        text-decoration: line-through;
+      }
+    }
+  }
+</style>
